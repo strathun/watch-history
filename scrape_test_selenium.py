e547778ad6caa3551533bf3d96fc1abf240cb6b1
strathun <hstrathman@gmail.com>
2022-03-18 07:28:06 -0600

    Add initial code from tinkernut tutorial.

---
+++ scrape_test_selenium.py
@@ -0,0 +1,46 @@
+# Copied from Tinkernut's video:
+# https://www.youtube.com/watch?v=tRNwTXeJ75U&t=18s
+#from bs4 import BeautifulSoup
+#import requests
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+import getpass
+from selenium.common.exceptions import NoSuchElementException
+
+#IF USING A RASPBERRY PI, FIRST INSTALL THIS OPTIMIZED CHROME DRIVER
+#sudo apt-get install chromium-chromedriver
+browser_driver = Service('/usr/lib/chromium-browser/chromedriver')
+page_to_scrape = webdriver.Chrome(service=browser_driver)
+page_to_scrape.get("http://quotes.toscrape.com")
+
+page_to_scrape.find_element(By.LINK_TEXT, "Login").click()
+
+
+time.sleep(3) # wait 3 seconds for login to load
+username = page_to_scrape.find_element(By.ID, "username")
+password = page_to_scrape.find_element(By.ID, "password")
+username.send_keys("admin")
+# to fully automate password/login, find some sort of python keyring instead to store
+my_pass = getpass.getpass()
+password.send_keys(my_pass)
+page_to_scrape.find_element(By.CSS_SELECTOR, "input.btn-primary").click()
+
+file = open("scraped_quotes.csv", "w")
+writer = csv.writer(file)
+
+writer.writerow(["QUOTES", "AUTHORS"])
+
+while True:
+    quotes = page_to_scrape.find_elements(By.CLASS_NAME, "text")
+    authors = page_to_scrape.find_elements(By.CLASS_NAME, "author")
+    for quote, author in zip(quotes, authors):
+        print(quote.text + " - " + author.text)
+        writer.writerow([quote.text, author.text])
+    try:
+        page_to_scrape_find_element(By.PARTIAL_LINK_TEXT, "Next").click()
+    except NoSuchElementException:
+        break
+file.close()
