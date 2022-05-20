e3594444d46363faeca612c2e65188a38dbd99e8
strathun <hstrathman@gmail.com>
2022-05-20 07:37:24 -0600

    Updated structure to load 'all' data first and then put this data into a dataframe to allow manipulation. Then save all of this to a csv.

--- scrape_netflix.py
+++ scrape_netflix.py
@@ -5,7 +5,10 @@ from selenium.webdriver.common.by import By
 import time
 from selenium.common.exceptions import NoSuchElementException
 import numpy as np
-
+import pandas as pd
+from os.path import exists
+#Eventually go "headless" when don't need to watch everything happen anymore.
+# This will reduce load times.
 #IF USING A RASPBERRY PI, FIRST INSTALL THIS OPTIMIZED CHROME DRIVER
 #sudo apt-get install chromium-chromedriver
 browser_driver = Service('/usr/lib/chromium-browser/chromedriver')
@@ -38,26 +41,48 @@ page_to_scrape.find_element(By.LINK_TEXT, "Account").click()
 page_to_scrape.find_element(By.ID, "profile_3").click()
 page_to_scrape.find_element(By.XPATH, '//*[@id="profile_3"]/ul/li[5]/a/div[1]').click()
 
-file = open("scraped_activity.csv", "w")
-writer = csv.writer(file)
+dates = []
+titles = []
 
-writer.writerow(["DATE", "TITLE"])
-
-date = page_to_scrape.find_elements(By.CLASS_NAME, "date")
-title = page_to_scrape.find_elements(By.CLASS_NAME, "title")
-for date, title in zip(date, title):
-    print(date.text + title.text)
-    writer.writerow([date.text, title.text])
-file.close()
-
-# while True:
-#     quotes = page_to_scrape.find_elements(By.CLASS_NAME, "text")
-#     authors = page_to_scrape.find_elements(By.CLASS_NAME, "author")
-#     for quote, author in zip(quotes, authors):
-#         print(quote.text + " - " + author.text)
-#         writer.writerow([quote.text, author.text])
-#     try:
-#         page_to_scrape.find_element(By.PARTIAL_LINK_TEXT, "Next").click()
-#     except NoSuchElementException:
-#         break
-# file.close()
+if exists("scraped_activity_netlix.csv"):
+    print('Not first run')
+    # working with dataframes now. Next steps:
+    # figure out why not everything on the loaded page is being scraped
+    # get this if statement going:
+        # copy mostly everything from below, but have it first load the last
+        # entry in the csv file and press the button until this shows up on screen
+        # THEN! after this matches, grab all the data on screen, convert it to the dataframe
+        # delete the matched entry and everything after it. Invert it, and then
+        # append it to the previously created csv (need to look up how to do this).
+else:
+    print('First data grab. Could take a while')
+    # Grab ALL the data if it's the first run. Test tomorrow by removing counters
+    file = open("scraped_activity_netlix.csv", "w")
+    #writer = csv.writer(file)
+    #writer.writerow(["DATE", "TITLE"])
+    test_counter = 0 # temporary counter to keep it from loading all previous data
+    while True:
+        test_counter = test_counter + 1
+        ## Previous method that works. 
+        #for date, title in zip(date, title):
+            #print(date.text + title.text)
+            #writer.writerow([date.text, title.text])
+        try:
+            page_to_scrape.find_element(By.CLASS_NAME, "btn-blue").click()
+        except NoSuchElementException:
+            break
+        if test_counter > 2:
+            break
+    date = page_to_scrape.find_elements(By.CLASS_NAME, "date")
+    title = page_to_scrape.find_elements(By.CLASS_NAME, "title")
+    #new method
+    time.sleep(3)
+    for date, title in zip(date, title):
+        dates.append(date.text)
+        titles.append(title.text)
+    dates.reverse()
+    titles.reverse()
+    new_data = pd.DataFrame({'DATE': dates, 'TITLE': titles})
+    new_data.to_csv('scraped_activity_netlix.csv', index=False, encoding='utf-8')
+    print(new_data)
+    file.close()
